50add7c8f0414a84928ea4402bb40304333d1e4e
pkluiber <pkluiber@gmail.com>
2021-09-22 15:57:58 +0200

    Klui

---
+++ 1 Exercises/scrpet.js
@@ -0,0 +1,12 @@
+var a = "Hello from the Console";
+var b = "Hello from the Browser";
+
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+
+
+console.log(typeof a);
+document.write(b);
+console.log(c);
+document.write(c); // Name
+console.log(players[2])
---
+++ 2. Exercises/car.js
@@ -0,0 +1,24 @@
+const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari", ];
+cars.sort();
+
+console.log(cars)
+
+
+var fruits = ["Apfel", "Banane", "Kiwi", ]
+fruits.splice(3, 0, "Orange")
+
+console.log(fruits)
+
+
+var animals = ["dog", "horse", "monkey", ];
+animals.sort();
+animals.splice(0, 0, "cat")
+
+console.log(animals)
+
+
+var web = ["mango", "cherries ", "kiwi ", "grapes", "pear ", "peach ", "orange", "lemon"];
+let text = ""
+list: {
+    text < br >
+}
